668e630373bd0c7f857c4532397f2f7bfc95c187
banteg <banteeg@gmail.com>
2017-04-25 11:37:14 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='fast.com',
+    version='1.0.0',
+    description='fast.com speedtest cli',
+    url='https://github.com/banteg/fast',
+    author='banteg',
+    licence='MIT',
+    py_modules=['fast'],
+    install_requires=['aiohttp'],
+    entry_points={
+        'console_scripts': [
+            'fast=fast:main',
+        ]
+    }
+)
